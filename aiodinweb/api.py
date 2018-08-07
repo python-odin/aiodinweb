89cf209107bc5814b88712988571546ef6e1a10d
Tim Savage <tim@savage.company>
2018-08-07 22:59:53 +1000

    Added base API

---
+++ aiodinweb/api.py
@@ -0,0 +1,10 @@
+"""
+AIO Odin Web
+~~~~~~~~~~~~
+
+Web APIs utilising Odin for encoding/decoding and validation.
+
+"""
+__authors__ = "Tim Savage"
+__author_email__ = "tim@savage.company"
+__copyright__ = "Copyright (C) 2018 Tim Savage"
