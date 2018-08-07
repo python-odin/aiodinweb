c9179bc2df77259ce174af972bc13b759539cc06
Tim Savage <tim@savage.company>
2018-08-07 22:59:39 +1000

    Added Request/Response classes

---
+++ aiodinweb/bases.py
@@ -0,0 +1,13 @@
+from aiohttp import web
+
+
+class Request(web.Request):
+    """
+    OdinWeb extended Request object
+    """
+
+
+class Response(web.Response):
+    """
+    OdinWeb extended Response object
+    """
