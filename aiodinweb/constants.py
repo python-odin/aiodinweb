4f2103b7d488e8a6600d0541e52c18d6c7f6d1af
Tim Savage <tim@savage.company>
2018-08-07 22:59:05 +1000

    Added additional constants

--- aiodinweb/constants.py
+++ aiodinweb/constants.py
@@ -7,13 +7,13 @@ class Method(enum.Enum):
     """
     HTTP Request Method
     """
-    GET = 'GET'
-    POST = 'POST'
-    PUT = 'PUT'
-    DELETE = 'DELETE'
-    HEAD = 'HEAD'
-    OPTIONS = 'OPTIONS'
-    PATCH = 'PATCH'
+    Get = 'GET'
+    Put = 'PUT'
+    Post = 'POST'
+    Delete = 'DELETE'
+    Options = 'OPTIONS'
+    Head = 'HEAD'
+    Patch = 'PATCH'
 
     def __eq__(self, value: Union[str, 'Method']) -> bool:
         if isinstance(value, str):
@@ -24,3 +24,37 @@ class Method(enum.Enum):
     def __hash__(self):
         return hash(self.value)
 
+
+class In(enum.Enum):
+    """
+    The location of the parameter.
+    """
+    Query = 'query'
+    Header = 'header'
+    Path = 'path'
+    Cookie = 'cookie'
+
+
+class DataType(enum.Enum):
+    """
+    Primitive data types in the OAS are based on the types supported.
+    """
+    Integer = ('integer', 'int32')
+    Long = ('integer', 'int64')
+    Float = ('number', 'float')
+    Double = ('number', 'double')
+    String = ('string', None)
+    Byte = ('string', 'byte')
+    Binary = ('string', 'binary')
+    Boolean = ('boolean', None)
+    Date = ('string', 'date')
+    DateTime = ('string', 'date-time')
+    Password = ('string', 'password')
+
+    @property
+    def type(self):
+        return self.value[0]
+
+    @property
+    def format(self):
+        return self.value[1]
