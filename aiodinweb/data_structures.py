0b2f4a97796cd30e6e95761c25e762d938d37c58
Tim Savage <tim@savage.company>
2018-08-17 00:06:12 +1000

    Added middleware list

--- aiodinweb/data_structures.py
+++ aiodinweb/data_structures.py
@@ -1,8 +1,9 @@
 import re
 
-from typing import Union, Sequence, Iterable, Callable
+from typing import Union, Sequence, Iterable, Callable, Optional, Any, Awaitable
 
 from . import constants
+from .web import Request, Response
 
 
 class Parameter:
@@ -208,3 +209,42 @@ class UrlPath(tuple):
 
 EmptyPath = UrlPath()
 RootPath = UrlPath('')
+
+
+RequestHandler = Callable[[Request, 'RequestHandler'], Awaitable[Response]]
+DispatchHandler = Callable[[Request, 'DispatchHandler'], Awaitable[Any]]
+Handle500Handler = Callable[[Request, Exception], Optional[Any]]
+OpenApiHandler = Callable[[], Optional[Any]]
+
+
+class MiddlewareList(list):
+    """
+    List of middleware with filtering and sorting builtin
+    """
+    @property
+    def request(self) -> Sequence[RequestHandler]:
+        """
+        List of request middleware methods
+        """
+        return tuple(m.request for m in self if hasattr(m, 'request'))
+
+    @property
+    def dispatch(self) -> Sequence[DispatchHandler]:
+        """
+        List of dispatch middleware methods.
+        """
+        return tuple(m.request for m in self if hasattr(m, 'dispatch'))
+
+    @property
+    def handle_500(self) -> Sequence[Handle500Handler]:
+        """
+        List of 500 error handler methods.
+        """
+        return tuple(m.request for m in self if hasattr(m, 'handle_500'))
+
+    @property
+    def openapi(self) -> Sequence[OpenApiHandler]:
+        """
+        List of OpenAPI methods.
+        """
+        return tuple(m.request for m in self if hasattr(m, 'openapi'))
