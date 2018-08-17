7c910a159d7234d791a88fae6b4d422f02d15c3c
Tim Savage <tim@savage.company>
2018-08-17 14:48:56 +1000

    Fleshed out operation

--- aiodinweb/operation.py
+++ aiodinweb/operation.py
@@ -1,9 +1,12 @@
-from aiohttp import web
-from typing import Any, Callable, Awaitable, Union, Sequence, Dict, Set, Iterable, Tuple
+from functools import partial
 
+from aiohttp import web
+from typing import Callable, Awaitable, Union, Sequence, Set, Iterable, Tuple, Dict, Any
+
+from aiodinweb.utils import cached_property
 from . import constants
-from .data_structures import Parameter, UrlPath
-from .utils.sequences import force_tuple
+from .data_structures import Parameter, UrlPath, MiddlewareList
+from .utils.sequences import force_tuple, dict_filter
 from .web import Request, Response
 
 
@@ -19,37 +22,83 @@ class Operation:
     """
     Decorator class that wraps API operations
     """
+    _operation_count = 0
+
     def __init__(self, func: OperationFunction, path: UrlPath.Atoms, *,
                  methods: Methods=constants.Method.Get,
                  tags: Union[str, Sequence[str]]=None,
                  summary: str=None,
-                 description: str=None,
-                 operation_id: str=None,
-                 deprecated: bool = False
-                 ) -> None:
+                 middleware: Sequence[object]=None) -> None:
         """
         :param func: Function that executes the operation.
         :param path: URL path to the operation.
         :param methods: HTTP method(s) this operation response to.
         """
         self.base_func = self.func = func
-        self.path = UrlPath.from_object(path)
+        self._path = UrlPath.from_object(path)
         self.methods = force_tuple(methods)
-        self.tags = set(force_tuple(tags)) if tags else EmptySet
-        self.summary = summary
-        self.description = description or func.__doc__
-        self.operation_id = operation_id or func.__name__
-        self.deprecated = deprecated
 
-        self.parameters: Set[Parameter] = set()
-        self.request_body = None
-        self.responses: Dict[int, Any] = None
+        # Sorting/hashing
+        self.sort_key = Operation._operation_count
+        Operation._operation_count += 1
+
+        # Parent object (when associated with an API Container)
+        self.parent = None
+        # Binding when part of an API class
+        self.binding = None
+
+        self.middleware = MiddlewareList(middleware or [])
+        self.middleware.append(self)  # Add self as middleware to obtain pre-dispatch support
+
+        # Security
         self.security = None
 
-        self.parent = None
+        # Documentation
+        self._tags = set(force_tuple(tags)) if tags else EmptySet
+        self.summary = summary
+        self.deprecated = False
+        self.parameters: Set[Parameter] = set()
+
+        # Copy values from function (if defined)
+        for attr in ('deprecated', 'parameters', 'security'):
+            try:
+                value = getattr(func, attr)
+            except AttributeError:
+                pass
+            else:
+                setattr(self, attr, value)
 
     async def __call__(self, request: Request) -> Response:
-        return await self.func(request)
+        """
+        Main wrapper around the operation function.
+        """
+        # Apply middleware
+        handler = self.func
+        for middleware in self.middleware.dispatch:
+            handler = partial(middleware, handler=handler)
+
+        return await handler(request)
+
+    def __eq__(self, other: 'Operation') -> bool:
+        if isinstance(other, Operation):
+            return all(
+                getattr(self, a) == getattr(other, a)
+                for a in ('path', 'methods')
+            )
+        return NotImplemented
+
+    def __str__(self) -> str:
+        return "{} - {} {}".format(self.operation_id, '|'.join(m for m in self.methods), self.path)
+
+    def __repr__(self) -> str:
+        return f"Operation({self.operation_id!r}, {self.path!r}, {self.methods})"
+
+    def bind_to_instance(self, instance: object) -> None:
+        """
+        Bind a ResourceApi instance to an operation.
+        """
+        self.binding = instance
+        self.middleware.append(instance)
 
     def op_paths(self, path_base: UrlPath.Atoms=None) -> Iterable[Tuple[UrlPath, 'Operation']]:
         """
@@ -59,3 +108,47 @@ class Operation:
         if path_base:
             url_path = path_base / url_path
         yield url_path, self
+
+    @cached_property
+    def path(self) -> UrlPath:
+        return self._path
+
+    @cached_property
+    def is_bound(self) -> bool:
+        """
+        Operation is bound to a API Class
+        """
+        return bool(self.binding)
+
+    # Docs ##########################################################
+
+    def to_openapi(self) -> Dict[str, Any]:
+        """
+        Generate OpenAPI documentation
+        """
+        return dict_filter(
+            operationId=self.operation_id,
+        )
+
+    @cached_property
+    def operation_id(self) -> str:
+        """
+        Unique identifier of a specific operation
+        """
+        func = self.base_func
+        value = getattr(func, 'operation_id', None)
+        return value or f"{func.__module__}.{func.__name__}"
+
+    @property
+    def tags(self) -> Set[str]:
+        """
+        Tags associated with this operation
+        """
+        tags = set()
+        if self._tags:
+            tags.update(self._tags)
+        if self.binding:
+            binding_tags = getattr(self.binding, 'tags', None)
+            if binding_tags:
+                tags.update(binding_tags)
+        return tags
