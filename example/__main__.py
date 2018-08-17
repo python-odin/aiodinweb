2fde2629abea21b31e11d965309b3e6aa30fec8d
Tim Savage <tim@savage.company>
2018-08-18 01:12:40 +1000

    Expanded example

--- example/__main__.py
+++ example/__main__.py
@@ -1,11 +1,18 @@
 from aiohttp import web
-from aiodinweb.aiohttp import AioApiInterface
+from aiodinweb import api
+from aiodinweb.openapi import OpenApiSpec
 
-from .api import example_api
+from .api import example_api, second_api
 
 
 app = web.Application()
-AioApiInterface(
-    example_api
-).setup(app)
+
+api_v1 = api.ApiVersion(
+    example_api,
+    second_api,
+    OpenApiSpec("Example", enable_ui=True),
+)
+
+api.CORS(api.AioApi(api_v1), ['http://localhost']).add_routes(app)
+
 web.run_app(app)
--- example/api.py
+++ example/api.py
@@ -1,11 +1,11 @@
 from aiodinweb import api
 
-example_api = api.ApiCollection()
+example_api = api.ApiCollection(name='example')
 
 
 @example_api.listing
 async def example_listing(request: api.Request):
-    pass
+    raise NotImplementedError()
 
 
 @example_api.create
@@ -14,15 +14,25 @@ async def example_create(request: api.Request):
 
 
 @example_api.detail
-async def example_detail(request: api.Request, resource_id: int):
-    pass
+async def example_detail(request: api.Request):
+    raise api.PermissionDenied("Uh uh aah!")
 
 
 @example_api.update
-async def example_update(request: api.Request, resource_id: int):
+async def example_update(request: api.Request):
     pass
 
 
 @example_api.delete
-async def example_delete(request: api.Request, resource_id: int):
+async def example_delete(request: api.Request):
     pass
+
+
+second_api = api.ApiCollection(name='second')
+
+
+@second_api.listing
+async def second_listing(request: api.Request):
+    return [
+        {"foo": 'bar'}
+    ]
