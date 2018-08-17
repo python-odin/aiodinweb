73430bbae43a9175c928e00e8371376029acaf21
Tim Savage <tim@savage.company>
2018-08-18 01:12:08 +1000

    Added sorting to middleware

--- aiodinweb/middleware/__init__.py
+++ aiodinweb/middleware/__init__.py
@@ -1,4 +1,4 @@
-from typing import Callable, Awaitable, Any, Optional, Sequence
+from typing import Callable, Awaitable, Any, Optional, Sequence, List, Iterable, TypeVar
 
 from ..web import Request, Response
 
@@ -6,6 +6,15 @@ RequestHandler = Callable[[Request, 'RequestHandler'], Awaitable[Response]]
 DispatchHandler = Callable[[Request, 'DispatchHandler'], Awaitable[Any]]
 Handle500Handler = Callable[[Request, Exception], Optional[Any]]
 OpenApiHandler = Callable[[], Optional[Any]]
+
+T = TypeVar('T')
+
+
+def sort_by_priority(iterable: Iterable[T], reverse: bool=False, default_priority: int=10) -> List[T]:
+    """
+    Return a list or objects sorted by a priority value.
+    """
+    return sorted(iterable, reverse=reverse, key=lambda o: getattr(o, 'priority', default_priority))
 
 
 class MiddlewareList(list):
@@ -17,25 +26,37 @@ class MiddlewareList(list):
         """
         List of request middleware methods
         """
-        return tuple(m.handle_request for m in self if hasattr(m, 'handle_request'))
+        return tuple(
+            m.handle_request for m in sort_by_priority(self)
+            if hasattr(m, 'handle_request')
+        )
 
     @property
     def dispatch(self) -> Sequence[DispatchHandler]:
         """
         List of dispatch middleware methods.
         """
-        return tuple(m.handle_dispatch for m in self if hasattr(m, 'handle_dispatch'))
+        return tuple(
+            m.handle_dispatch for m in sort_by_priority(self)
+            if hasattr(m, 'handle_dispatch')
+        )
 
     @property
     def handle_500(self) -> Sequence[Handle500Handler]:
         """
         List of 500 error handler methods.
         """
-        return tuple(m.handle_500 for m in self if hasattr(m, 'handle_500'))
+        return tuple(
+            m.handle_500 for m in sort_by_priority(self)
+            if hasattr(m, 'handle_500')
+        )
 
     @property
     def openapi(self) -> Sequence[OpenApiHandler]:
         """
         List of OpenAPI methods.
         """
-        return tuple(m.handle_openapi for m in self if hasattr(m, 'handle_openapi'))
+        return tuple(
+            m.handle_openapi for m in sort_by_priority(self)
+            if hasattr(m, 'handle_openapi')
+        )
