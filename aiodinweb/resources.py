a73d1f968fc0709a8041bb71710e758112593050
Tim Savage <tim@savage.company>
2018-08-19 01:21:54 +1000

    Converted Error to resource

--- aiodinweb/resources.py
+++ aiodinweb/resources.py
@@ -1,23 +1,41 @@
+import odin
+
 from http import HTTPStatus
-from typing import Any, Dict
+from typing import Any
 
 
-class Error:
-    @staticmethod
-    def from_status(http_status: HTTPStatus, code_index: int=0, message: str=None,
-                    developer_message: str=None, meta: Any=None) -> Dict[str, Any]:
+class Error(odin.Resource):
+    """
+    Standard error response
+    """
+    status = odin.IntegerField(verbose_name="HTTP status code")
+    code = odin.IntegerField(verbose_name="Error code")
+    message = odin.StringField(verbose_name="End user message")
+    developer_message = odin.StringField(verbose_name="Developer message", null=True)
+    meta = odin.DictField(verbose_name='Error specific metadata', null=True)
+
+    @classmethod
+    def from_status(
+            cls, status: HTTPStatus, *,
+            code_index: int=0,
+            message: str=None,
+            developer_message: str=None,
+            meta: Any=None
+    ) -> 'Error':
         """
         Automatically build an HTTP Response form HTTP Status code.
 
-        :param http_status: HTTP Status code
+        :param status: HTTP Status code
         :param code_index: Unique status code index.
         :param message: End user message
         :param developer_message: Developer message
         :param meta: Additional error metadata
 
         """
-        return dict(status=http_status,
-                    code=(http_status * 100) + code_index,
-                    message=message or http_status.phrase,
-                    developer_message=developer_message or http_status.description,
-                    meta=meta)
+        return cls(
+            status=status,
+            code=(status * 100) + code_index,
+            message=message or status.phrase,
+            developer_message=developer_message or status.description,
+            meta=meta
+        )
